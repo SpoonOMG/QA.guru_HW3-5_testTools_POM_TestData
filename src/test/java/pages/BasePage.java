787230576bac965940dd05672d4807ea87c6f604
Михайлюк Андрей <andrey.mikhaylyuk@gazprombank.ru>
2023-01-15 11:45:20 +0300

    Added Selenoid connection

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -3,6 +3,7 @@ package pages;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -16,6 +17,10 @@ public class BasePage {
     @BeforeAll
     static void selenoidConnection(){
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", "true");
+        capabilities.setCapability("enableVideo", "true");
+        Configuration.browserCapabilities = capabilities;
     }
     @BeforeEach
     void launchBrowserTest(){
@@ -31,6 +36,9 @@ public class BasePage {
     }
     @AfterEach
     void attachments(){
-
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
